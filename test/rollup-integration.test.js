6920a8795430e58306ad0d7e59896a8997e7f489
Ari Perkkiö <ari.perkkio@gmail.com>
2022-09-10 12:46:58 +0000

    test: rollup integration

---
+++ test/rollup-integration.test.js
@@ -0,0 +1,49 @@
+import { rmSync, writeFileSync } from "fs";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { rollup } from "rollup";
+
+import { localImport } from "../index";
+
+const plugin = localImport();
+
+const input = "input.js";
+const output = { file: "output.js" };
+const source = `
+// ExportAllDeclaration
+export * from "./local-file";
+export * from "../file-from-parent-directory";
+export * from 'some-dependency';
+`.trim();
+
+beforeAll(() => {
+  writeFileSync(input, source, "utf-8");
+});
+
+afterAll(() => {
+  rmSync(input);
+  rmSync(output.file);
+});
+
+test("rollup", async () => {
+  const build = await rollup({
+    input,
+    external: () => true,
+    plugins: [
+      // TODO: Rollup tries to serialize plugin.
+      // Figure out why `plugins: [localImport()]` fails to serialize.
+      {
+        name: plugin.name,
+        transform: (...args) => plugin.transform(...args),
+      },
+    ],
+  });
+
+  const bundle = await build.write({ output });
+
+  expect(bundle.output).toHaveLength(1);
+  expect(bundle.output[0].code.trim()).toMatchInlineSnapshot(`
+    "export * from './local-file.js';
+    export * from '../file-from-parent-directory.js';
+    export * from 'some-dependency';"
+  `);
+});
