0edfb69d037c732c7787085cd836f473048780cc
Ari Perkkiö <ari.perkkio@gmail.com>
2022-09-17 11:25:22 +0000

    feat: replace napi-rs with neon

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,41 @@
+use neon::prelude::*;
+
 mod parser;
 
-pub fn transform(source_code: String) -> TransformResult {
-    let transformed = parser::parse(&source_code);
-
-    // TODO: Include source map
-    TransformResult { code: transformed }
+struct TransformResult {
+    pub code: String,
 }
 
-pub struct TransformResult {
-    pub code: String,
+impl TransformResult {
+    // https://neon-bindings.com/docs/objects#converting-rust-data-to-javascript
+    fn to_object<'a>(&self, cx: &mut FunctionContext<'a>) -> JsResult<'a, JsObject> {
+        let obj = cx.empty_object();
+
+        let code = cx.string(&self.code);
+        obj.set(cx, "code", code)?;
+
+        Ok(obj)
+    }
+}
+
+fn transform(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let source_code = cx.argument::<JsString>(0)?;
+    let transformed = parser::parse(&source_code.value(&mut cx));
+
+    // TODO: Include source map
+    let result = TransformResult { code: transformed };
+
+    let output = result.to_object(&mut cx)?;
+
+    Ok(output)
+}
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    let name: Handle<JsString> = cx.string("local-import");
+    cx.export_value("name", name)?;
+
+    cx.export_function("transform", transform)?;
+
+    Ok(())
 }
