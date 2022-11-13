2093f3d3e5d282eeb75f215b1475d93b8705bff8
Ari Perkkiö <ari.perkkio@gmail.com>
2022-11-13 15:49:56 +0000

    feat(error-handling): callback reference failures - Throw useful error message when callback referencing fails

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,14 @@ pub struct TransformResult {
 #[napi]
 pub struct Plugin {
     pub name: String,
-    callback: Ref<()>,
+    callback_reference: Ref<()>,
 }
 
 #[napi]
 impl Plugin {
     #[napi]
     pub fn transform(&self, env: Env, source_code: String) -> TransformResult {
-        let callback: JsFunction = env.get_reference_value(&self.callback).unwrap();
+        let callback: JsFunction = env.get_reference_value(&self.callback_reference).unwrap();
 
         let transform_paths = Box::new(move |path: String| {
             let args: [JsString; 1] = [env.create_string(&path).unwrap()];
@@ -42,7 +42,7 @@ impl Plugin {
 
     #[napi]
     pub fn build_end(&mut self, env: Env) -> Result<(), Error> {
-        if let Err(_) = self.callback.unref(env) {
+        if let Err(_) = self.callback_reference.unref(env) {
             return Err(Error::new(
                 napi::Status::GenericFailure,
                 String::from("Failed to cleanup callback. Unexpected Rollup lifecycle order."),
@@ -54,9 +54,19 @@ impl Plugin {
 }
 
 #[napi(ts_args_type = "callback: (path: string) => string")]
-pub fn local_import(env: Env, callback: JsFunction) -> Plugin {
-    Plugin {
+pub fn local_import(env: Env, callback: JsFunction) -> Result<Plugin, Error> {
+    let callback_reference = match env.create_reference(callback) {
+        Ok(reference) => reference,
+        Err(_) => {
+            return Err(Error::new(
+                napi::Status::GenericFailure,
+                String::from("Failed to reference callback. Did you pass function to `localImport(callback)`?"),
+            ));
+        }
+    };
+
+    Ok(Plugin {
         name: String::from("local-import"),
-        callback: env.create_reference(callback).unwrap(),
-    }
+        callback_reference,
+    })
 }
--- test/error-handling.test.js
+++ test/error-handling.test.js
@@ -12,3 +12,15 @@ test("throws useful error message when callback cleanup fails", () => {
     '"Failed to cleanup callback. Unexpected Rollup lifecycle order."'
   );
 });
+
+test("throws useful error message when callback is missing", () => {
+  expect(() => localImport()).toThrowErrorMatchingInlineSnapshot(
+    '"Failed to reference callback. Did you pass function to `localImport(callback)`?"'
+  );
+});
+
+test("throws useful error message when callback is not function", () => {
+  expect(() => localImport(123)).toThrowErrorMatchingInlineSnapshot(
+    '"Failed to reference callback. Did you pass function to `localImport(callback)`?"'
+  );
+});
