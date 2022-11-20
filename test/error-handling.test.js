2d82825f29a4f178d133235b8f21b37919c3bd01
Ari Perkkiö <ari.perkkio@gmail.com>
2022-11-20 12:33:35 +0000

    test: add test case for erroneous callback

--- test/error-handling.test.js
+++ test/error-handling.test.js
@@ -1,6 +1,12 @@
-import { expect, test } from "vitest";
+import { afterEach, expect, test } from "vitest";
 
 import { localImport } from "../plugin";
+
+const cleanups = [];
+
+afterEach(() => {
+  cleanups.splice(0).forEach((cleanup) => cleanup());
+});
 
 test("throws useful error message when callback cleanup fails", () => {
   const plugin = localImport(() => {});
@@ -23,4 +29,15 @@ test("throws useful error message when callback is not function", () => {
   expect(() => localImport(123)).toThrowErrorMatchingInlineSnapshot(
     '"Failed to reference callback. Did you pass function to `localImport(callback)`?"'
   );
+});
+
+test.todo("throws useful error message when callback throws", () => {
+  const plugin = localImport(() => {
+    throw new Error("Error from callback");
+  });
+  cleanups.push(plugin.buildEnd);
+
+  expect(() =>
+    plugin.transform('import fs from "./some-file";')
+  ).toThrowErrorMatchingInlineSnapshot();
 });
